b0c09326db0da9b80fa1c4e101ff34486f8c2bfb
elyelvo <eldosampo@gmail.com>
2024-10-23 09:26:27 -0700

    week two assignment

---
+++ plp.py
@@ -0,0 +1,13 @@
+my_list =[]
+
+my_list = [10,20,30,40]
+
+my_list.insert(1,15)
+
+my_list.extend([50,60,70])
+
+my_list.remove (70)
+my_list.sort()
+index_of_30 = my_list.index(30)
+my_list, index_of_30
+print(my_list)
